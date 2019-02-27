51c2bcb087100a12ea9e08d2e5647da6a88b3066
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-02-27 11:17:37 +0300

    removing the curl call

--- build.sh
+++ build.sh
@@ -7,13 +7,16 @@ docker-compose up -d
 
 scripts/wait-for-it.sh -p 8080 -h localhost -- echo "Webserver is up"
 
-curl -X GET \
-    http://localhost:8080/admin/demultiplex/health \
-    -H 'Cache-Control: no-cache' \
-    -H 'Content-Type: application/json'
-
 
 # Just show some logs here
 docker-compose logs --tail 100 airflow_sequencer_automation_webserver
 docker-compose logs --tail 100 airflow_sequencer_automation_scheduler
 docker-compose logs --tail 100 airflow_sequencer_automation_worker
+
+
+## Not totally sure why this fails
+#curl -X GET \
+#    http://localhost:8080/admin/demultiplex/health \
+#    -H 'Cache-Control: no-cache' \
+#    -H 'Content-Type: application/json'
+
