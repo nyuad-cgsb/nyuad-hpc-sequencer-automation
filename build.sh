0a4e5d16ce42fd09c1602fa85d359916b7d5f31f
Jillian Rowe <jillian.e.rowe@gmail.com>
2019-02-27 10:29:53 +0300

    adding in docker-compose logs to instance

--- build.sh
+++ build.sh
@@ -10,3 +10,9 @@ curl -X GET \
     http://localhost:8080/admin/demultiplex/health \
     -H 'Cache-Control: no-cache' \
     -H 'Content-Type: application/json'
+
+
+# Just show some logs here
+docker-compose logs --tail 100 airflow_sequencer_automation_webserver
+docker-compose logs --tail 100 airflow_sequencer_automation_scheduler
+docker-compose logs --tail 100 airflow_sequencer_automation_worker
